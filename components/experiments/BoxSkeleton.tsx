9f88d703321f881f5b8ed0054294048b0c0082e4
remcostoeten <remcostoeten@hotmail.com>
2023-04-18 20:03:06 +0200

    Remove whitespace.

--- components/experiments/BoxSkeleton.tsx
+++ components/experiments/BoxSkeleton.tsx
@@ -4,7 +4,7 @@ import Bars from './Bars';
 export default function BoxSkeleton() {
 	return (
 		<>
-			<div className='container	'>
+			<div className='container'>
 				<div className='py-5 px-5 : border-solid border-2 rounded-lg border-gray-200 container mx-auto my-40'>
 					<div className='box max-w-2xl border-'>
 						<div className='heading flex justify-between items-center pb-8'>
