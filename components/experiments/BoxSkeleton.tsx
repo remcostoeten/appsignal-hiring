d7820f888df4ee1bcb2709df71f8e956414ab240
remcostoeten <remcostoeten@hotmail.com>
2023-04-18 19:39:31 +0200

    Add box wrapper skeleton in tailwind.

---
+++ components/experiments/BoxSkeleton.tsx
@@ -0,0 +1,21 @@
+import React from 'react';
+import Link from 'next/link';
+
+export default function BoxSkeleton() {
+	return (
+		<>
+			<div className='container	'>
+				<div className='py-5 px-5 : border-solid border-2 rounded-lg border-gray-200 container mx-auto my-40'>
+					<div className='box max-w-2xl border-'>
+						<div className='heading flex justify-between items-center pb-8'>
+							<h2 className='text-lg font-bold'>Box Heading</h2>
+							<Link className='text-blue-500 underline' href='#'>
+								Link
+							</Link>
+						</div>
+					</div>
+				</div>
+			</div>
+		</>
+	);
+}
---
+++ pages/experiments.jsx
@@ -0,0 +1,11 @@
+// small page for myself to try/test some things regarding tailwind etc.without breaking/bloating the main files. In prod I obviously would not commit such file but for this  application it might be usefull.
+import React from 'react';
+import BoxSkeleton from '../components/experiments/BoxSkeleton';
+
+export default function test() {
+	return (
+		<>
+			<BoxSkeleton />
+		</>
+	);
+}
