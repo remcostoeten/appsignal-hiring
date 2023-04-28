3d516f644ecc259402474c32481c496d7771ed0f
remcostoeten <remcostoeten@hotmail.com>
2023-04-28 16:41:13 +0200

    Add v2 which is not hardcoded and add tradeoff comment.

--- components/MetadataDistributions.test.jsx
+++ components/MetadataDistributions.test.jsx
@@ -5,22 +5,38 @@ import dataFromGraphql from '@/data/graphql';
 import userEvent from '@testing-library/user-event'; // a package which stimulates user events. NPM package can be deleted prior to merging to prod branch if not needed.
 
 describe('MetadataDistributions component', () => {
-	test('The distribution "hostname" is rendered properly ', () => {
+	// v1
+	// Simple test to check if both titles from the graphql data are actually rendered since they are not hardcoded unlike "Attribute distributions" which would be useless to test.
+
+	test('dhe distribution "hostname" is rendered properly ', () => {
 		render(
 			<MetadataDistributions
 				distributions={dataFromGraphql.app.metadataDistributions}
 			/>,
 		);
-		// Simple test to check if both titles from the graphql data are actually rendered since they are not hardcoded unlike "Attribute distributions" which would be useless to test.
+
 		expect(screen.getByText('hostname')).toBeInTheDocument();
 	});
+
 	test('The distribution  "revision" is rendered properly ', () => {
 		render(
 			<MetadataDistributions
 				distributions={dataFromGraphql.app.metadataDistributions}
 			/>,
 		);
-		// Simple test to check if both titles from the graphql data are actually rendered since they are not hardcoded unlike "Attribute distributions" which would be useless to test.
-		expect(screen.getByText('revision')).toBeInTheDocument();
+
+		expect(screen.getByText('hostname')).toBeInTheDocument();
+	});
+
+	// v2 
+	// A more cleaner approach but the problem here is that it will pass regardless of typos coming from the data,  but it will fail if the data is not present in the graphql query. It's a tradeoff which can be discussed based on the amount of data/components that are going to be tested.
+	const distributions = dataFromGraphql.app.metadataDistributions;
+
+	distributions.forEach((distribution) => {
+		test(`The distribution "${distribution.name}" is rendered properly`, () => {
+			render(<MetadataDistributions distributions={[distribution]} />);
+
+			expect(screen.getByText(distribution.name)).toBeInTheDocument();
+		});
 	});
 });
